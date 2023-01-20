df8eb1cf0c8e41b709021b2c047fdd00d1f757b0
JohanAnquetil <114992907+JohanAnquetil@users.noreply.github.com>
2023-01-20 15:36:10 +0100

    firts commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: BasicsPage(),
+    );
+  }
+}
+
+class BasicsPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+   var size = MediaQuery.of(context).size;
+   var platform = Theme.of(context).platform;
+   print("size: $size");
+   print("platform: $platform");
+   return Scaffold(
+     appBar: AppBar(
+       title: const Text('REFUGEES WELCOME !'),
+       backgroundColor: Colors.indigo,
+       leading: IconButton(
+         onPressed: () {},
+         icon: const Icon(Icons.home),
+       ),
+       actions: <Widget>[
+         IconButton(
+           icon: const Icon(Icons.menu),
+           tooltip: 'Show Snackbar',
+           onPressed: () {
+             ScaffoldMessenger.of(context).showSnackBar(
+                 const SnackBar(content: Text('LES KEUFS ARRIVENT !')));
+           },
+         ),
+
+       ],
+     ),
+     body: Container(
+       height: size.height,
+       color: Color.fromRGBO(194, 231, 255, 1),
+       child: Center(
+         child: Image.asset(
+       "images/banner.jpeg"
+       ),
+       )
+     ),
+   );
+  }
+}
